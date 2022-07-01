7fd524538e1f1052164bb8f08d6b2df49a52195f
Sumio K <tap.miwo@gmail.com>
2022-07-01 22:42:25 +0900

    adding deletemethod

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,35 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-
+  
   def show
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
   end
-
+  
   def new
   end
-
+  
   def create
-    @post = Post.new(content:params[:content])
-    @post.save 
+    @post = Post.new(content: params[:content])
+    @post.save
     redirect_to("/posts/index")
   end
-
+  
   def edit
-    @post = Post.find_by(id:params[:id])
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
+  def destroy
+    @post =Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
   end
 
 end
