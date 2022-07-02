4db5ef3c4ebc809cf59dfe9a53902b6cbec34d4b
Sumio K <tap.miwo@gmail.com>
2022-07-02 20:19:42 +0900

    complete users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get "users/:id" => "users#show"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
