9086aabf993dbc71eb06077d9fc218a026e6c52b
Sumio K <tap.miwo@gmail.com>
2022-07-02 20:50:43 +0900

    add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
-  
+
+  def new
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new"
+
+
   get 'users/index'
   get "users/:id" => "users#show"
+
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+  
 
   get "/" => "home#top"
   get "about" => "home#about"
