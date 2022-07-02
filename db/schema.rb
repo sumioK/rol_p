c90fda455027f4364a45734cc56552d9cdac60bb
Sumio K <tap.miwo@gmail.com>
2022-07-02 19:39:18 +0900

    add new db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_204403) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_02_102619) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
