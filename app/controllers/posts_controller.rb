8594c1659ffa3ccb7c479e4bd54b46131aba7c77
Sumio K <tap.miwo@gmail.com>
2022-07-03 20:33:49 +0900

    access limit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user, {only:[:edit, :update, :index, :show]}
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
