f849e8a877dba2f717893274a096649d36a8740b
Sumio K <tap.miwo@gmail.com>
2022-07-04 06:54:46 +0900

    complete ensure_correct_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user, {only:[:edit, :update, :index, :show]}
+  before_action :ensure_correct_user ,{only:[:edit , :update, :destroy]}
   
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -48,5 +49,14 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to("/posts/index")
   end
+
+  def ensure_correct_user
+    @post =Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+  
 
 end
