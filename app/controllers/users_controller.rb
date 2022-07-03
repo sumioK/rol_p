7af8f9dbb96ec24763cadc7b50a498104419c27a
Sumio K <tap.miwo@gmail.com>
2022-07-03 15:33:42 +0900

    begin loginsys

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,15 @@ class UsersController < ApplicationController
   end
 
   def login
-
+    @user = User.find_by(email: params[:email] , password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else 
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]      
+      render("login_form")
+    end
   end
 end
