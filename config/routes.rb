f74e7adc2a9f28b65f418aecc43c166ed7c98eff
Sumio K <tap.miwo@gmail.com>
2022-07-04 21:43:34 +0900

    add remove likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
 
   get "login" => "users#login_form"
   post "login" => "users#login"
